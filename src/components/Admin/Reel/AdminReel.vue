acf8a8ab981da8017d94b8ef1c7e655ceb43c215
anhnvz <anhnvz@nec.vn>
2024-07-02 11:45:32 +0700

    fix: update

--- src/components/Admin/Reel/AdminReel.vue
+++ src/components/Admin/Reel/AdminReel.vue
@@ -2,22 +2,6 @@
   <div class="flex flex-col gap-[24px]">
     <h1 class="text-headline-5">Reel Lists</h1>
 
-    <div class="bg-white p-[24px] rounded-lg">
-      <!-- <Table
-        v-if="dataSource"
-        :columns="columns"
-        :row-key="(record) => record.login.uuid"
-        :data-source="dataSource"
-        :pagination="pagination"
-        :loading="loading"
-        @change="handleTableChange"
-      >
-        <template #bodyCell="{ column, text }">
-          <template v-if="column.dataIndex === 'name'">
-            <a href="admin/reel/:reelId">{{ text.title }}</a></template
-          >
-        </template>
-      </Table> -->
-    </div>
+    <div class="bg-white p-[24px] rounded-lg"></div>
   </div>
 </template>
