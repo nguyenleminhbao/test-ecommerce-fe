27096e031af410024af470de3c26773a3aaaee94
Bao <baonlmz@nec.vn>
2024-07-02 10:52:31 +0700

    deploy v4

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,17 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting',
-    '@vue/typescript/recommended'
+    '@vue/eslint-config-prettier/skip-formatting'
+    // '@vue/typescript/recommended'
   ],
+  rules: {
+    'vue/no-parsing-error': 'error'
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   }
